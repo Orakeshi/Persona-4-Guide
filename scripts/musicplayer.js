1356b9388d5a8b79abbcff734f230e4f01048a82
Tommy Webb <73914490+Orakeshi@users.noreply.github.com>
2021-01-11 16:09:05 +0000

    Ver 1.03

---
+++ scripts/musicplayer.js
@@ -0,0 +1,61 @@
+document.addEventListener('play', function(checker){ //Event Listener function that listens for play events
+    var audios = document.getElementsByTagName('audio'); //Get all Audio elements on page
+    for(var i = 0, len = audios.length; i < len;i++){ //if audios playing
+        if(audios[i] != checker.target){ //If audio element in location 1 is not playing || target function finds the element that triggered the event
+            audios[i].pause(); //pause audio element
+            audios[i].currentTime = 0; //reset song to start
+        }
+    }
+}, true);
+
+function playSong(clicked){
+    var song = document.querySelector('#song-'+String(clicked))
+    var character = document.getElementById("rise-singing")
+    var characterStill = document.getElementById("rise-singing-still")
+    if (song.paused) {
+        song.play();
+        character.style.visibility = "visible";
+        characterStill.style.visibility = "hidden";
+    } 
+    else 
+    {
+        song.pause();
+        characterStill.style.visibility = "visible";
+        character.style.visibility = "hidden";
+    }
+}
+
+
+
+/*
+function pauseSong(){
+    var audios = document.getElementsByTagName('audio'); //Get all Audio elements on page
+    for(var i = 0, len = audios.length; i < len;i++){ //if audios length is greater than 0 add 1 to counter
+        if(audios[i] != pauseSong.target){ //If audio element in location 1 is not playing || target function finds the element that triggered the event
+            audios[i].pause();
+        }
+    }
+}
+window.addEventListener("DOMContentLoaded", event =>{
+    const audio = document.querySelector("audio");
+    audio.volume = 0.2;
+    audio.pause();
+});
+
+
+function playSong(clicked){
+    
+    if(clicked=="btn-shadow-world"){
+        const song = document.querySelector('#song'.concat(clicked));
+        song.play();
+        
+    }
+
+    else if (clicked=="btn-snowflakes"){
+        const song = document.querySelector('#song'.concat(clicked));
+        song.play();
+    }
+
+}
+*/
+
